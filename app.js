c1a706326f4e7bd8d25e43a227cf825dd3076b0f
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-12 06:41:24 -0700

    wrote function to switch between icons

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const sections = document.querySelectorAll('.section');
 const divButtons = document.querySelectorAll('.controls');
 const buttons = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+
+function PageTransition() {
+  // Button click changes active class
+  for (let i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+      let currentButton = document.querySelectorAll('.active-btn');
+      currentButton[0].className = currentButton[0].className.replace('active-btn', '');
+      this.className += ' active-btn';
+      console.log(this);
+    })
+  }
+}
+
+PageTransition();
