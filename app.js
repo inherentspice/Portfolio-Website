a15c005a47e149b8cc88f71395fcccd70cc3f0b8
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-14 04:42:29 -0700

    added animations between page transitions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const divButtons = document.querySelectorAll('.controls');
 const buttons = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 
 function PageTransition() {
@@ -11,9 +11,30 @@ function PageTransition() {
       let currentButton = document.querySelectorAll('.active-btn');
       currentButton[0].className = currentButton[0].className.replace('active-btn', '');
       this.className += ' active-btn';
-      console.log(this);
     })
   }
+
+  console.log(allSections);
+  //Changing the active section
+  allSections.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //remove active class from other buttons
+      buttons.forEach((button) => {
+        button.classList.remove('active');
+      })
+      e.target.classList.add('active');
+
+      //hide other sections from view
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      })
+
+      const element = document.getElementById(id)
+      console.log(id)
+      element.classList.add('active');
+    }
+  })
 }
 
 PageTransition();
